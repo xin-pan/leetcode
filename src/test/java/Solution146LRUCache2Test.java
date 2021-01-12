7696b4510233c38f5ce23a00d789e976c0c2f10c
panxin <xinp_5@163.com>
2021-01-12 23:16:28 +0800

    Solution146LRUCache2Test

---
+++ src/test/java/Solution146LRUCache2Test.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Solution146LRUCache2Test {
+    @Test
+    public void should_get_negative_when_empty() {
+        assertEquals(-1, new Solution146LRUCache2(1).get(1));
+    }
+
+    @Test
+    public void should_get_negative_when_exceeding_capacity() {
+        Solution146LRUCache2 lruCache = new Solution146LRUCache2(2);
+        lruCache.put(1, 1);
+        assertEquals(1, lruCache.get(1));
+        lruCache.put(2, 2);
+        assertEquals(1, lruCache.get(1));
+        lruCache.put(3, 3);
+        assertEquals(1, lruCache.get(1));
+        assertEquals(-1, lruCache.get(2));
+        assertEquals(3, lruCache.get(3));
+    }
+
+}
