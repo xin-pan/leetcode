dea078dd2bbf4fbceff65ca83513c31a26e4787e
panxin <xinp_5@163.com>
2021-01-13 15:28:36 +0800

    Solution146LRUCache5 should_get_when_cached

--- src/main/java/leetcode/Solution146LRUCache5.java
+++ src/main/java/leetcode/Solution146LRUCache5.java
@@ -1,9 +1,18 @@
 package leetcode;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution146LRUCache5 {
 
+    private Map<Integer, Integer> cache = new HashMap<>();
+
     public int get(int key) {
-        return -1;
+        return this.cache.getOrDefault(key, -1);
+    }
+
+    public void put(int key, int value) {
+        this.cache.put(key, value);
     }
 }
--- src/test/java/leetcode/Solution146LRUCache5Test.java
+++ src/test/java/leetcode/Solution146LRUCache5Test.java
@@ -9,4 +9,11 @@ public class Solution146LRUCache5Test {
     public void should_get_negative_when_empty() {
         assertEquals(-1, new Solution146LRUCache5().get(1));
     }
+
+    @Test
+    public void should_get_when_cached() {
+        Solution146LRUCache5 lruCache = new Solution146LRUCache5();
+        lruCache.put(1, 1);
+        assertEquals(1, lruCache.get(1));
+    }
 }
