82cbf02a036d65e0504f53a09b7e323c33a51006
panxin <xinp_5@163.com>
2021-01-13 11:11:20 +0800

    Solution146LRUCache4: should_get_when_cached

--- src/main/java/leetcode/Solution146LRUCache4.java
+++ src/main/java/leetcode/Solution146LRUCache4.java
@@ -1,8 +1,17 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution146LRUCache4 {
 
+    private Map<Integer, Integer> cache = new HashMap<>();
+
     public int get(int key) {
-        return -1;
+        return cache.getOrDefault(1, -1);
+    }
+
+    public void put(int key, int value) {
+        cache.put(key, value);
     }
 }
--- src/test/java/leetcode/Solution146LRUCache4Test.java
+++ src/test/java/leetcode/Solution146LRUCache4Test.java
@@ -10,4 +10,10 @@ public class Solution146LRUCache4Test {
         assertEquals(-1, new Solution146LRUCache4().get(1));
     }
 
+    @Test
+    public void should_get_when_cached() {
+        Solution146LRUCache4 solution146LRUCache = new Solution146LRUCache4();
+        solution146LRUCache.put(1, 1);
+        assertEquals(1, solution146LRUCache.get(1));
+    }
 }
