24c9bd2aca0cf692ffa7334c85b8e79ef4a77133
panxin <xinp_5@163.com>
2021-01-13 10:29:29 +0800

    Solution146LRUCache3: metaphor<Temperature>

--- src/main/java/leetcode/Solution146LRUCache3.java
+++ src/main/java/leetcode/Solution146LRUCache3.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class Solution146LRUCache3 {
     private final int capacity;
     private final static Node DEFAULT_NODE = new Node(-1, -1);
-    private DoubleLinkedList temperater = new DoubleLinkedList();
+    private final Temperature temperature = new Temperature();
     private Map<Integer, Node> cache = new HashMap();
 
     public Solution146LRUCache3(int capacity) {
@@ -16,22 +16,22 @@ public class Solution146LRUCache3 {
     public int get(int key) {
         if (cache.containsKey(key)) {
             Node oldNode = cache.get(key);
-            Node hottest = temperater.addAndMarkAsHotest(key, oldNode.value);
+            Node hottest = temperature.addAndMarkAsHottest(key, oldNode.value);
             cache.put(key, hottest);
         }
         return cache.getOrDefault(key, DEFAULT_NODE).value;
     }
 
     public void put(int key, int value) {
-        Node hottest = temperater.addAndMarkAsHotest(key, value);
+        Node hottest = temperature.addAndMarkAsHottest(key, value);
         cache.put(key, hottest);
     }
 
-    private class DoubleLinkedList {
+    private class Temperature {
         Node head;
         Node tail;
 
-        public Node addAndMarkAsHotest(int key, int value) {
+        public Node addAndMarkAsHottest(int key, int value) {
             Node node = new Node(key, value);
             node.next = head;
 
