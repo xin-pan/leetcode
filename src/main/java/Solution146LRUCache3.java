801819c07982120c843a8677a1ac1b27b126fa48
panxin <xinp_5@163.com>
2021-01-13 09:48:09 +0800

    Solution146LRUCache3: should_get_cached_value

--- src/main/java/Solution146LRUCache3.java
+++ src/main/java/Solution146LRUCache3.java
@@ -1,11 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution146LRUCache3 {
     private final int capacity;
+    private Map<Integer, Integer> cache = new HashMap();
 
     public Solution146LRUCache3(int capacity) {
         this.capacity = capacity;
     }
 
     public int get(int key) {
-        return -1;
+        return cache.getOrDefault(key, -1);
+    }
+
+    public void put(int key, int value) {
+        cache.put(key, value);
     }
 }
--- src/test/java/Solution146LRUCache3Test.java
+++ src/test/java/Solution146LRUCache3Test.java
@@ -8,4 +8,12 @@ public class Solution146LRUCache3Test {
         assertEquals(-1, new Solution146LRUCache3(1).get(1));
     }
 
+    @Test
+    public void should_get_cached_value() {
+        Solution146LRUCache3 solution146LRUCache = new Solution146LRUCache3(1);
+        solution146LRUCache.put(1, 1);
+        solution146LRUCache.put(2, 2);
+        assertEquals(1, solution146LRUCache.get(1));
+        assertEquals(2, solution146LRUCache.get(2));
+    }
 }
