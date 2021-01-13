51e596b822cfab64532c2c088fa924770a1f0710
panxin <xinp_5@163.com>
2021-01-13 10:15:53 +0800

    Solution146LRUCache3 maintain DoubleLinkedList to track the temperature

--- src/main/java/leetcode/Solution146LRUCache3.java
+++ src/main/java/leetcode/Solution146LRUCache3.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class Solution146LRUCache3 {
     private final int capacity;
     private final static Node DEFAULT_NODE = new Node(-1, -1);
-    private DoubleLinkedList latestUsed = new DoubleLinkedList();
+    private DoubleLinkedList temperater = new DoubleLinkedList();
     private Map<Integer, Node> cache = new HashMap();
 
     public Solution146LRUCache3(int capacity) {
@@ -14,16 +14,37 @@ public class Solution146LRUCache3 {
     }
 
     public int get(int key) {
+        if (cache.containsKey(key)) {
+            Node oldNode = cache.get(key);
+            Node hottest = temperater.addAndMarkAsHotest(key, oldNode.value);
+            cache.put(key, hottest);
+        }
         return cache.getOrDefault(key, DEFAULT_NODE).value;
     }
 
     public void put(int key, int value) {
-        cache.put(key, new Node(key, value));
+        Node hottest = temperater.addAndMarkAsHotest(key, value);
+        cache.put(key, hottest);
     }
 
     private class DoubleLinkedList {
         Node head;
         Node tail;
+
+        public Node addAndMarkAsHotest(int key, int value) {
+            Node node = new Node(key, value);
+            node.next = head;
+
+            if (head == null) {
+                head = node;
+            } else {
+                head.prev = node;
+            }
+            if (tail == null) {
+                tail = node;
+            }
+            return node;
+        }
     }
 
     private static class Node {
