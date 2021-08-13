5d6eb6c1e330c6fb325fd819bb7da4ee3a09b07a
panxin <xinp_5@163.com>
2021-08-13 17:57:40 +0800

    Solution146LRUCache8 should_not_expire_lru_node_when_put_duplicate

--- src/test/java/leetcode/Solution146LRUCache8Test.java
+++ src/test/java/leetcode/Solution146LRUCache8Test.java
@@ -45,4 +45,13 @@ public class Solution146LRUCache8Test {
         lruCache.put(2, 3);
         assertEquals(3, lruCache.get(2));
     }
+    @Test
+    public void should_not_expire_lru_node_when_put_duplicate() {
+        Solution146LRUCache lruCache = new Solution146LRUCache8(2);
+        lruCache.put(1, 1);
+        lruCache.put(2, 2);
+        lruCache.put(2, 3);
+        assertEquals(1, lruCache.get(1));
+        assertEquals(3, lruCache.get(2));
+    }
 }
