ac6d8ca6d6f5992a7d756fe4ea5e2f56fc7af118
panxin <xinp_5@163.com>
2021-08-13 11:28:21 +0800

    Solution146LRUCache7 use DoubleLinkedList to implement

--- src/main/java/leetcode/Solution146LRUCache7.java
+++ src/main/java/leetcode/Solution146LRUCache7.java
@@ -1,16 +1,66 @@
 package leetcode;
 
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.Map;
 
 public class Solution146LRUCache7 extends Solution146LRUCacheAbstract {
     private Map<Integer, Node> cache = new HashMap<>();
-    private LinkedList<Node> sorted = new LinkedList<>();
+    private DoubleLinkedList sorted = new DoubleLinkedList();
+
+    class DoubleLinkedList {
+        Node first;
+        Node last;
+        private int size = 0;
+
+        Node removeFirst() {
+            Node oldFirst = this.first;
+            remove(oldFirst);
+            return oldFirst;
+        }
+
+        void remove(Node node) {
+            if (node.prev != null) {
+                // not first node
+                node.prev.next = node.next;
+            } else {
+                // first node
+                first = node.next;
+            }
+            if (node.next != null) {
+                // not last node
+                node.next.prev = node.prev;
+            } else {
+                // last node
+                last = node.prev;
+            }
+            size--;
+        }
+
+        void addLast(Node node) {
+            if (size == 0) {
+                first = node;
+                last = node;
+            } else {
+                // link
+                last.next = node;
+                node.prev = last;
+
+                // make the new node last one.
+                last = node;
+            }
+            size++;
+        }
+
+        public int size() {
+            return size;
+        }
+    }
 
     class Node {
         int key;
         int value;
+        Node prev;
+        Node next;
 
         public Node(int key, int value) {
             this.key = key;
