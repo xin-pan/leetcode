f493c518bdba7e6d039738b134fd61502b44ba01
panxin <xinp_5@163.com>
2021-08-13 17:22:04 +0800

    Solution146LRUCache8

---
+++ src/test/java/leetcode/Solution146LRUCache8Test.java
@@ -0,0 +1,40 @@
+package leetcode;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Solution146LRUCache8Test {
+
+    @Test
+    public void should_get_negative_when_out_of_cache() {
+        Solution146LRUCache lruCache = new Solution146LRUCache8(2);
+        lruCache.put(1, 1);
+        assertEquals(-1, lruCache.get(2));
+    }
+    @Test
+    public void should_get_negative_when_within_cache() {
+        Solution146LRUCache lruCache = new Solution146LRUCache8(2);
+        lruCache.put(1, 1);
+        assertEquals(1, lruCache.get(1));
+    }
+    @Test
+    public void should_get_negative_when_cache_expires() {
+        Solution146LRUCache lruCache = new Solution146LRUCache8(2);
+        lruCache.put(1, 1);
+        lruCache.put(2, 2);
+        lruCache.put(3, 3);
+        assertEquals(-1, lruCache.get(1));
+    }
+    @Test
+    public void should_expire_least_recently_used() {
+        Solution146LRUCache lruCache = new Solution146LRUCache8(3);
+        lruCache.put(1, 1);
+        lruCache.put(2, 2);
+        lruCache.put(3, 3);
+        lruCache.get(1);
+        lruCache.put(4, 4);
+        assertEquals(1, lruCache.get(1));
+        assertEquals(-1, lruCache.get(2));
+    }
+}
