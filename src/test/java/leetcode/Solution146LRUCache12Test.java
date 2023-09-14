588f9c2308fc340003ef3bcbc37724f081b7f9ac
PAN Xin <xinp_5@163.com>
2023-09-14 20:10:08 +0800

    Solution146LRUCache12Test: should_expire_least_recent_get_when_cache_hit_limit

--- src/test/java/leetcode/Solution146LRUCache12Test.java
+++ src/test/java/leetcode/Solution146LRUCache12Test.java
@@ -19,13 +19,25 @@ public class Solution146LRUCache12Test {
     }
 
     @Test
-    public void should_expire_least_recent_used_when_cache_hit_limit(){
+    public void should_expire_least_recent_put_when_cache_hit_limit(){
         var cache = new Solution146LRUCache12(2);
         cache.put("1", "1");
         cache.put("2", "2");
         cache.put("3", "3");
         assertNull(cache.get("1"));
         assertEquals("2", cache.get("2"));
+        assertEquals("3", cache.get("3"));
+    }
+
+    @Test
+    public void should_expire_least_recent_get_when_cache_hit_limit(){
+        var cache = new Solution146LRUCache12(2);
+        cache.put("1", "1");
+        cache.put("2", "2");
+        assertEquals("1", cache.get("1"));
+        cache.put("3", "3");
+        assertEquals("1", cache.get("1"));
+        assertNull(cache.get("2"));
         assertEquals("3", cache.get("3"));
     }
 }
