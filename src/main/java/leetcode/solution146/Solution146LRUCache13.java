fe04b334b2d67d478abd28d2dcc5bcd033c03a68
PAN Xin <xinp_5@163.com>
2023-09-14 21:05:48 +0800

    Solution146LRUCache13 should_get_value_when_cache_hit

--- src/main/java/leetcode/solution146/Solution146LRUCache13.java
+++ src/main/java/leetcode/solution146/Solution146LRUCache13.java
@@ -1,7 +1,17 @@
 package leetcode.solution146;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution146LRUCache13 {
+    public static final int DEFAULT_VALUE = -1;
+    Map<Integer, Integer> cache = new HashMap<>();
+
     public int get(int key) {
-        return -1;
+        return cache.getOrDefault(key, DEFAULT_VALUE);
+    }
+
+    public void put(int key, int value) {
+        cache.put(key, value);
     }
 }
--- src/test/java/leetcode/solution146/Solution146LRUCache13Test.java
+++ src/test/java/leetcode/solution146/Solution146LRUCache13Test.java
@@ -6,8 +6,15 @@ import static org.junit.Assert.assertEquals;
 
 public class Solution146LRUCache13Test {
     @Test
-    public void should_get_negative_when_cache_not_hit(){
+    public void should_get_negative_when_cache_not_hit() {
         var cache = new Solution146LRUCache13();
         assertEquals(-1, cache.get(1));
+    }
+
+    @Test
+    public void should_get_value_when_cache_hit() {
+        var cache = new Solution146LRUCache13();
+        cache.put(1, 1);
+        assertEquals(1, cache.get(1));
     }
 }
