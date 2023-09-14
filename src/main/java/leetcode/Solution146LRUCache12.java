a7c5b20b446e9b02974e71f6bd4e5d83281b3105
PAN Xin <xinp_5@163.com>
2023-09-14 19:47:12 +0800

    Solution146LRUCache12Test: should_get_value_when_cached

--- src/main/java/leetcode/Solution146LRUCache12.java
+++ src/main/java/leetcode/Solution146LRUCache12.java
@@ -1,7 +1,15 @@
 package leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution146LRUCache12 {
+    private Map<String, String> cache= new HashMap<>();
     public String get(String key) {
-        return null;
+        return cache.get(key);
+    }
+
+    public void put(String key, String value) {
+        cache.put(key, value);
     }
 }
--- src/test/java/leetcode/Solution146LRUCache12Test.java
+++ src/test/java/leetcode/Solution146LRUCache12Test.java
@@ -2,6 +2,7 @@ package leetcode;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
 public class Solution146LRUCache12Test {
@@ -9,5 +10,11 @@ public class Solution146LRUCache12Test {
     public void should_not_get_value_when_not_cached(){
         var solution146LRUCache12 = new Solution146LRUCache12();
         assertNull(solution146LRUCache12.get("1"));
+    }
+    @Test
+    public void should_get_value_when_cached(){
+        var solution146LRUCache12 = new Solution146LRUCache12();
+        solution146LRUCache12.put("1", "1");
+        assertEquals("1", solution146LRUCache12.get("1"));
     }
 }
