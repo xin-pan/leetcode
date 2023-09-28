3e1fc1065e8a888686991e9a7014d55283bd07f0
PAN Xin <xinp_5@163.com>
2023-09-28 10:36:08 +0800

    should_expire_head_when_exceeds_limit

--- src/main/java/practice/LRUSolution.java
+++ src/main/java/practice/LRUSolution.java
@@ -1,12 +1,24 @@
 package practice;
 
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 public class LRUSolution {
+    private final int sizeLimit;
     private Map<Integer, Integer> cache = new HashMap();
+    private List<Integer> queue = new LinkedList<>();
+
+    public LRUSolution(int sizeLimit) {
+        this.sizeLimit = sizeLimit;
+    }
 
     public void put(int key, int value) {
+        if (this.queue.size() >= sizeLimit) {
+            this.cache.remove(this.queue.remove(0));
+        }
+        this.queue.add(key);
         this.cache.put(key, value);
     }
 
--- src/test/java/practice/LRUSolutionTest.java
+++ src/test/java/practice/LRUSolutionTest.java
@@ -7,13 +7,21 @@ import static org.junit.Assert.assertEquals;
 public class LRUSolutionTest {
     @Test
     public void should_return_cached_when_hit() {
-        var cache = new LRUSolution();
+        var cache = new LRUSolution(1);
         cache.put(1, 1);
         assertEquals(1, cache.get(1));
     }
     @Test
     public void should_return_negative_when_not_hit() {
-        var cache = new LRUSolution();
+        var cache = new LRUSolution(1);
         assertEquals(-1, cache.get(1));
+    }
+    @Test
+    public void should_expire_head_when_exceeds_limit() {
+        var cache = new LRUSolution(1);
+        cache.put(1, 1);
+        cache.put(2, 2);
+        assertEquals(-1, cache.get(1));
+        assertEquals(2, cache.get(2));
     }
 }
