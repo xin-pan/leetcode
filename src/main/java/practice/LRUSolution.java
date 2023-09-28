077b7ba52a4ae9c1e0d81f65f17667c09f758497
PAN Xin <xinp_5@163.com>
2023-09-28 10:27:37 +0800

    init LRUSolution and its test

--- src/main/java/practice/LRUSolution.java
+++ src/main/java/practice/LRUSolution.java
@@ -1,38 +1,4 @@
 package practice;
 
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
 public class LRUSolution {
-    public static final int DEFAULT_SIZE = 8;
-    private final int sizeLimit;
-    private Map<Integer, Integer> cache;
-    private List<Integer> queue;
-
-    public LRUSolution() {
-        this(DEFAULT_SIZE);
-    }
-
-    public LRUSolution(int cacheSize) {
-        this.sizeLimit = cacheSize;
-        this.cache = new HashMap<>();
-        this.queue = new LinkedList<>();
-    }
-
-    public void put(Integer key, Integer value) {
-        if (cache.size() >= this.sizeLimit) {
-            this.cache.remove(this.queue.remove(0));
-        }
-        this.cache.put(key, value);
-        this.queue.add(key);
-    }
-
-    public int get(Integer key) {
-        this.queue = this.queue.stream().dropWhile(x -> x.intValue() == key.intValue()).collect(Collectors.toList());
-        this.queue.add(key);
-        return cache.getOrDefault(key, -1);
-    }
 }
