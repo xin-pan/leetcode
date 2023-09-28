048b204fdcdfeda38640e94a3d73928fd5b90a39
PAN Xin <xinp_5@163.com>
2023-09-28 10:31:29 +0800

    should_return_cached_when_hit

--- src/main/java/practice/LRUSolution.java
+++ src/main/java/practice/LRUSolution.java
@@ -1,4 +1,16 @@
 package practice;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LRUSolution {
+    private Map<Integer, Integer> cache = new HashMap();
+
+    public void put(int key, int value) {
+        this.cache.put(key, value);
+    }
+
+    public int get(int key) {
+        return this.cache.get(key);
+    }
 }
--- src/test/java/practice/LRUSolutionTest.java
+++ src/test/java/practice/LRUSolutionTest.java
@@ -1,4 +1,14 @@
 package practice;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class LRUSolutionTest {
+    @Test
+    public void should_return_cached_when_hit() {
+        var cache = new LRUSolution();
+        cache.put(1, 1);
+        assertEquals(1, cache.get(1));
+    }
 }
